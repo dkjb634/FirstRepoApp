0e44878cd2bdef686e9c77317665a1c0bd0ab11e
Dmitry.Kazantsev <dmitry.kazantsev@jetbrains.com>
2024-10-29 16:20:41 +0100

    Commit to two different repositories at once

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
-// Each file will be committed to an appropriate repository
+// Each file will be committed to an appropriate repositorysds
